f082704958df5b6ccbe0e1bd16ed9a9535564a50
BROTOsejati <153246371+BROTOsejati@users.noreply.github.com>
2023-12-08 09:56:24 +0700

    1

---
+++ index.php
@@ -0,0 +1,73 @@
+ <?php include 'header.php' ;?>
+   <div class="jumbotron jumbotron-fluid">
+    <div class="container text-center">
+      
+    <img class="rounded-circle" width="300" height="300" src="https://upload.wikimedia.org/wikipedia/en/thumb/4/43/Rangers_FC.svg/1200px-Rangers_FC.svg.png">
+      <h1 class="display-4">RANGERS FC</h1>
+      <p class="lead">RANGERS FC ADALAH KLUB ASAL SCOTLAND </p>
+    </div>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1>About</h2>
+      </div>
+
+      <div class="row">
+        <div class="col">
+          <p>Rangers Football Club adalah klub sepak bola di Glasgow, Skotlandia, yang bermain di Liga Utama Skotlandia, tingkat pertama Liga Sepak Bola Profesional Skotlandia. Tempat tinggal mereka, Stadion Ibrox, berada di barat daya kota.`</p>
+        </div>
+        <div class="col">
+          <p>Rangers telah memenangkan lebih banyak gelar liga dan treble dibanding klub lain di dunia, memenangkan gelar liga sebanyak 55 kali, Piala Skotlandia 33 kali dan Piala Liga Skotlandia 27 kali, dan mencapai treble ketiganya pada musim yang sama tujuh kali. Rangers adalah klub Inggris pertama yang mencapai final turnamen UEFA dan memenangkan Piala Winners UEFA pada tahun 1972 setelah menjadi runner-up dua kali pada tahun 1961 dan 1967.</p>
+        </div>
+      </div>
+    </div>
+  
+
+  </div>
+
+  
+
+  </div>
+
+  </div>
+  <div class="card-group">
+    <div class="card">
+      <img class="card-img-top" src="https://i.pinimg.com/736x/82/6e/05/826e05b75d4305bbfce89f4228e3f3d8.jpg" alt="Card image cap">
+  
+      <div class="card-body">
+        <h5 class="card-title">player</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Last updated 3 mins ago</small>
+      </div>
+    </div>
+    <div class="card">
+      <img class="card-img-top" src="https://i.pinimg.com/736x/82/6e/05/826e05b75d4305bbfce89f4228e3f3d8.jpg" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">key player</h5>
+        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Last updated 3 mins ago</small>
+      </div>
+    </div>
+    <div class="card">
+      <img class="card-img-top" src="https://i.pinimg.com/736x/82/6e/05/826e05b75d4305bbfce89f4228e3f3d8.jpg" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">key player</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Last updated 3 mins ago</small>
+      </div>
+    </div>
+  </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <?php include 'footer.php' ;?>
+  
